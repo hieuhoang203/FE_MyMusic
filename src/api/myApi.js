1f54fcadd60b786f06fbb9378b9a0493cc8b6798
hieuhv203 <hieuhv203@gmail.com>
2025-03-21 00:19:06 +0700

    update get all genres with new api

--- src/api/myApi.js
+++ src/api/myApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosHelper = axios.create({
-    baseURL: "http://localhost:8903",
+    baseURL: "http://localhost:8920",
+    // baseURL: "https://be-mymusic.onrender.com",
     headers: {
         "content-type": "application/json",
     },
